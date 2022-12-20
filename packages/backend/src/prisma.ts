7f276eb20df56008507aa2c2c6156056f026a71f
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2022-12-20 14:21:25 +0100

    chore: expand attributes
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/backend/src/prisma.ts
+++ packages/backend/src/prisma.ts
@@ -5,7 +5,20 @@ export const prisma = new PrismaClient();
 
 prisma.$use(async (params, next) => {
   return newrelic.startSegment(`${params.model}.${params.action}`, true, () => {
-    newrelic.addCustomSpanAttribute("db.args", params.args);
+    Object.entries(params.args).forEach(([key, value]) => {
+      switch (typeof value) {
+        case "string":
+        case "number":
+        case "boolean":
+          newrelic.addCustomSpanAttribute(`db.${key}`, value);
+          return;
+        case "undefined":
+          return;
+        default:
+          newrelic.addCustomSpanAttribute(`db.${key}`, JSON.stringify(value));
+          return;
+      }
+    });
     return next(params);
   });
 });
