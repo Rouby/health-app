f38118d8fa28bb0cafdc1b910b58a895ce410a53
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2022-12-20 12:41:40 +0100

    feat: add prisma tracing
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/backend/src/prisma.ts
+++ packages/backend/src/prisma.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import * as newrelic from "newrelic";
 
 export const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  return newrelic.startSegment(`${params.model}.${params.action}`, true, () => {
+    newrelic.addCustomSpanAttribute("args", params.args);
+    return next(params);
+  });
+});
