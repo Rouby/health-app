7299a3ffad6cc63aa92a886d2efd02283de9d9d1
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2023-05-18 12:30:46 +0200

    feat: instrument pino and prisma after-the-fact
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/backend/src/logger.ts
+++ packages/backend/src/logger.ts
@@ -1,5 +1,8 @@
 import nrPino from "@newrelic/pino-enricher";
+import newrelic from "newrelic";
 import pino from "pino";
+
+newrelic.instrumentLoadedModule("pino", pino);
 
 export const logger = pino(nrPino());
 logger.level = process.env.LOG_LEVEL || "info";
--- packages/backend/src/prisma.ts
+++ packages/backend/src/prisma.ts
@@ -1,24 +1,7 @@
+import * as prismaPkg from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
 import * as newrelic from "newrelic";
 
-export const prisma = new PrismaClient();
+newrelic.instrumentLoadedModule("@prisma/client", prismaPkg);
 
-prisma.$use(async (params, next) => {
-  return newrelic.startSegment(`${params.model}.${params.action}`, true, () => {
-    Object.entries(params.args).forEach(([key, value]) => {
-      switch (typeof value) {
-        case "string":
-        case "number":
-        case "boolean":
-          newrelic.addCustomSpanAttribute(`db.${key}`, value);
-          return;
-        case "undefined":
-          return;
-        default:
-          newrelic.addCustomSpanAttribute(`db.${key}`, JSON.stringify(value));
-          return;
-      }
-    });
-    return next(params);
-  });
-});
+export const prisma = new PrismaClient();
