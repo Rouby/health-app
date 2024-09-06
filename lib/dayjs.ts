034e5550d1d0366a78ea92b5f8d85f133d03b882
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2024-09-06 18:30:31 +0200

    feat: guess timezone
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- lib/dayjs.ts
+++ lib/dayjs.ts
@@ -3,14 +3,16 @@ import duration from "dayjs/plugin/duration";
 import minMax from "dayjs/plugin/minMax";
 import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 import weekday from "dayjs/plugin/weekday";
 
 dayjs.extend(duration);
 dayjs.extend(minMax);
 dayjs.extend(relativeTime);
 dayjs.extend(weekday);
+dayjs.extend(utc);
 dayjs.extend(timezone);
 
-dayjs.tz.setDefault("Europe/Berlin");
+dayjs.tz.setDefault(dayjs.tz.guess() ?? "Europe/Berlin");
 
 export { dayjs };
